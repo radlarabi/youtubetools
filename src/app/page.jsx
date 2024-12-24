8cf0552812f25ae2402a3ebac03de9e44b4ac1f1
radouan larabi <116183732+radlarabi@users.noreply.github.com>
2024-12-24 15:02:21 +0000

    change to linkedin search page

---
+++ src/app/page.jsx
@@ -0,0 +1,171 @@
+'use client'
+
+import { useState } from "react";
+import LoadingSkeleton from '@/component/LoadingSkeleton'
+
+// import Image from 'next/image';
+// import FacebookIcon from '../assets/svg/facebook.svg'
+// import InstagramIcon from '../assets/svg/instagram.svg'
+// import YoutubeIcon from '../assets/svg/youtube.svg'
+
+// const url = 'https://linkedin-api8.p.rapidapi.com/search-jobs?keywords=react&locationId=92000000&datePosted=anyTime&sort=mostRelevant';
+
+
+export default function Home() {
+  const [topic, setTopic] = useState(null)
+  const [result, setResult] = useState('')
+  const [visited, setVisited] = useState({})
+  const [loading, setLoading] = useState(false)
+
+	console.log(process.env.X_RAPIDAPI_KEY, process.env.X_RAPIDAPI_HOST)
+
+	const options = {
+		method: 'GET',
+		headers: {
+			'x-rapidapi-key': process.env.X_RAPIDAPI_KEY,
+			'x-rapidapi-host': process.env.X_RAPIDAPI_HOST,
+		}
+	};
+  const url = 'https://linkedin-api8.p.rapidapi.com/search-jobs?keywords=' + topic + '&locationId=102787409&datePosted=anyTime&sort=mostRelevant';
+  
+  async function getData(){
+	setLoading(true)
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setResult(result)
+      
+      console.log(result);
+		setLoading(false)
+	} catch (error) {
+		console.error(error);
+		setLoading(false)
+    }
+  }
+
+
+
+  return (
+	<div className="h-100 w-100">
+
+		<div className="mx-auto w-[25rem] m-[4rem]">
+			<label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+			<div className="relative">
+				<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+					<svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+						<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+					</svg>
+				</div>
+				
+				<input onChange={(e) => setTopic(e.target.value)} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Jobs on Linkedin" required />
+				<button disabled={!topic}  onClick={() => getData()} type="submit" className={` ${!topic ? 'bg-gray-600' : 'bg-blue-500'} text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>Search</button>
+			</div>
+		</div>
+		{
+			loading ? <LoadingSkeleton/> : 
+			<div className="flex flex-col justify-center items-center result-container">
+				{
+					result && result?.data?.map(el => (
+					<div key={el.id} className="text-white my-2">
+						<a href={el.url} className="url" target={`__blank${el.id}`}>
+						<div className={`${visited[el.id] ? 'text-blue-300' : ''} `} onClick={() => setVisited(prev => ({...prev, [el.id]: true}))}>{el.title}</div>
+						</a>
+					</div>
+					))
+				}
+			</div>
+		}
+	</div>
+  )
+}
+
+  {/* <div className={styles.description}>
+  <a href="/about">go to about</a>
+  <a href="/contact">go to contact</a>
+
+    <p>
+      Get started by editing&nbsp;
+      <code className={styles.code}>src/app/page.js</code>
+    </p>
+    <div>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        By{' '}
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className={styles.vercelLogo}
+          width={100}
+          height={24}
+          priority
+        />
+      </a>
+    </div>
+  </div>
+
+  <div className={styles.center}>
+    <Image
+      className={styles.logo}
+      src="/next.svg"
+      alt="Next.js Logo"
+      width={180}
+      height={37}
+      priority
+    />
+  </div>
+
+  <div className={styles.grid}>
+    <a
+      href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className={styles.card}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2>
+        Docs <span>-&gt;</span>
+      </h2>
+      <p>Find in-depth information about Next.js features and API.</p>
+    </a>
+
+    <a
+      href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className={styles.card}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2>
+        Learn <span>-&gt;</span>
+      </h2>
+      <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+    </a>
+
+    <a
+      href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className={styles.card}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2>
+        Templates <span>-&gt;</span>
+      </h2>
+      <p>Explore the Next.js 13 playground.</p>
+    </a>
+
+    <a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className={styles.card}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2>
+        Deploy <span>-&gt;</span>
+      </h2>
+      <p>
+        Instantly deploy your Next.js site to a shareable URL with Vercel.
+      </p>
+    </a>
+  </div> */}
---
+++ src/test/Youtube/page.jsx
@@ -0,0 +1,100 @@
+// 'use client'
+// import { useState } from 'react'
+// import React from 'react'
+// import axios from 'axios'
+
+// function getYoutubeVideoId(url) {
+//   try {
+//     const youtubeUrl = new URL(url);
+//     let videoId;
+
+//     if (youtubeUrl.hostname === "youtu.be") {
+      
+//       videoId = youtubeUrl.pathname.substr(1);
+    
+//     } else if (youtubeUrl.hostname === "www.youtube.com" && youtubeUrl.searchParams.has("v")) {
+      
+//       videoId = youtubeUrl.searchParams.get("v");
+    
+//     } else {
+      
+//       throw new Error("Invalid YouTube URL");
+//     }
+
+//     return videoId.trim() !== "" ? videoId : null;
+//   } catch (error) {
+//     console.error("Invalid YouTube URL" + error);
+//     return null;
+//   }
+// }
+
+// function getAudioUrl(result){
+//   const len = result.adaptiveFormats.length;
+//   const format = result.adaptiveFormats
+//   let i = 0
+//   while(i < len){
+//     if (format[i].audioQuality == "AUDIO_QUALITY_MEDIUM")
+//       return format[i].url;
+//     i++;
+//   }
+//   return "youtubetools.vercel.app"
+// }
+
+// function Youtube() {
+//   const [result, setResult] = useState(null)
+//   const [link, setLink] = useState(null)
+//   const thumbnailLink = `https://img.youtube.com/vi/${getYoutubeVideoId(link)}/maxresdefault.jpg`
+//   const options = {
+//     method: 'GET',
+//     url: 'https://ytstream-download-youtube-videos.p.rapidapi.com/dl',
+//     params: {id: `${getYoutubeVideoId(link)}`},
+//     headers: {
+//       'X-RapidAPI-Key': 'b6a833b38dmsh59568ba84b92151p16851bjsnb0044221bc34',
+//       'X-RapidAPI-Host': 'ytstream-download-youtube-videos.p.rapidapi.com'
+//     }
+//   };
+//   const getData = async () => {
+//     try{
+//       const response = await axios.request(options);
+//       console.log(response)
+//       setResult(response.data)
+//     }
+//     catch(e){
+//       console.err(e)
+//     }
+//   }
+//   return (
+//     <div className='flex justify-center items-center h-screen flex-wrap'> 
+//         <div className="flex flex-wrap justify-center">
+//           <input className='w-full rounded-md py-1.5 pl-7 pr-20 text-gray-900 ring-1 border-solid border-black border-2  my-3' type="text" 
+//             onChange={(e) => {setLink(e.target.value); setResult(null)}} />
+          
+//           <button className='bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:border-blue-300' 
+//             onClick={getData}>Download</button>
+//         </div>
+//         {
+//           result && result.formats ? 
+//           <div className='flex flex-col justify-center '>
+//             <div className='flex-raw'>
+//               <p className='my-5'>{result.title}</p>
+//               <img className='' src={thumbnailLink} alt="" srcset="" width={640} height={380}/>
+//             </div>
+//             <div className='flex flex-col my-3 justify-center'>
+//               <a className='bg-blue-500 text-white px-4 py-2 my-2 w-fit rounded-lg hover:bg-blue-600 focus:outline-none focus:border-blue-300' 
+//                 href={result.formats[1].url } download target='__blank'>Download Video</a>
+
+//               <a className='bg-blue-500 text-white px-4 py-2 my-2 w-fit rounded-lg hover:bg-blue-600 focus:outline-none focus:border-blue-300' 
+//                 href={getAudioUrl(result)} download target='__blank'>Download Audio</a>
+
+//               <a className='bg-blue-500 text-white px-4 py-2 my-2 w-fit rounded-lg hover:bg-blue-600 focus:outline-none focus:border-blue-300' 
+//                 href={thumbnailLink } download target='__blank'>Download thumbnail</a>
+//             </div>
+//           </div>
+//           :
+//           <div></div>
+//         }
+//     </div>
+//   )
+// }
+
+// export default Youtube
